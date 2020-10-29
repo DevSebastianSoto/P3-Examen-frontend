f9901532f5b4dc4bfba9033c1daa497a8ab95465
Sebastian Soto Madrigal <s.m.sebastian.n@gmail.com>
2020-10-28 18:26:19 -0600

    sidebar navigation

--- src/app/blocks/sidebar/sidebar.component.ts
+++ src/app/blocks/sidebar/sidebar.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
+  public sideLinks = [
+    { icon: 'fas fa-users', path: '', text: 'Trabajadores' },
+    {
+      icon: 'fas fa-network-wired',
+      path: '/tipos-activos',
+      text: 'Tipos de activos',
+    },
+    { icon: 'fas fa-desktop', path: '/activos', text: 'Activos' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
